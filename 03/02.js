d97d51dc2197ea7ea441afa5c6bca597b0887eb8
arminasi <armanminasyanc@gmail.com>
2022-07-10 04:53:50 -0700

    uncompleted

---
+++ 03/02.js
@@ -0,0 +1,5 @@
+//2. Given an array of numbers, padding amount and repeat count. 
+// Pad the array in the following way: the padding amount specifies 
+// how many elements should be taken from the array edges, the repeat
+// amount specifies how many times the pad should be repeated.
+// Also, you should check that  padding amount <= length of array.
